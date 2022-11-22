2fcc8a393b15bb02f620c0efffb1111351ee2528
cociobanu <cciobanu@straighterline.com>
2022-11-22 13:52:15 +0200

    last changes

--- ShopService/build.sh
+++ ShopService/build.sh
@@ -7,7 +7,6 @@ sleep 20
 
 statement='CREATE USER "replication"@"%" IDENTIFIED WITH mysql_native_password BY "slave123"; GRANT REPLICATION SLAVE ON *.* TO "replication"@"%"';
 docker exec mysql_master sh -c "mysql -u root -pMastermaster123 -e '$statement'"
-docker exec mysql_master sh -c "mysql -u root -pMastermaster123 -e 'CREATE DATABASE store;'"
 MS_STATUS=`docker exec mysql_master sh -c "mysql -u root -pMastermaster123 -e 'SHOW MASTER STATUS;'"`
 CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
 CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
@@ -16,3 +15,5 @@ echo $CURRENT_POS
 
 docker exec mysql_slave sh -c "mysql -u root -pSlaveslave123 -e \"CHANGE MASTER TO MASTER_HOST='mysql_master', MASTER_USER='replication', MASTER_PASSWORD='slave123', MASTER_LOG_FILE='$CURRENT_LOG', MASTER_LOG_POS=$CURRENT_POS; START SLAVE;\""
 docker exec mysql_slave sh -c "mysql -u root -pSlaveslave123 -e 'SHOW SLAVE STATUS \G'"
+
+docker exec mysql_master sh -c "mysql -u root -pMastermaster123 -e 'CREATE DATABASE store;'"
--- ShopService/src/main/java/com/shopservice/ShopService/routing/RoutingDataSource.java
+++ ShopService/src/main/java/com/shopservice/ShopService/routing/RoutingDataSource.java
@@ -1,10 +1,14 @@
 package com.shopservice.ShopService.routing;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import java.util.logging.Logger;
 
 public class RoutingDataSource extends AbstractRoutingDataSource {
     @Override
     protected Object determineCurrentLookupKey() {
+        logger.info(RoutingDataSourceContext.getDataSourceRoutingKey());
         return RoutingDataSourceContext.getDataSourceRoutingKey();
     }
 }
