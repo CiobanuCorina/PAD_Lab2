9f5af63e6e5354c2ed627372aa0203533353701f
cociobanu <cciobanu@straighterline.com>
2022-12-13 10:37:07 +0200

    test pipeline

--- ShopService/src/test/java/com/shopservice/ShopService/controller/ProductControllerTest.java
+++ ShopService/src/test/java/com/shopservice/ShopService/controller/ProductControllerTest.java
@@ -11,7 +11,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
